409afc77a17debfe81528e43df6e63bf52a63aa8
VladislavDegtyarenko <vladislavdegtyarenko@gmail.com>
2020-12-06 15:35:59 +0200

    desktop ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
    const burgerMenuButton = document.querySelector('.nav__burger');
-   const aboutCards = document.querySelectorAll('.about__card')
+   const aboutCards = document.querySelectorAll('.about__card');
 
    burgerMenuButton.addEventListener('click', () => {
       burgerMenuButton.firstElementChild.classList.toggle('nav__burger_inner-active');
@@ -10,8 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
       card.addEventListener('click', function() {
          /* aboutCards.forEach(cardItem => {
             cardItem.classList.remove('about__card-clicked');
-         })
- */
+         }) */
          card.classList.toggle('about__card-clicked');
       })
    })
@@ -19,53 +18,131 @@ document.addEventListener('DOMContentLoaded', () => {
    const contactsButton = document.querySelector('#contactsButton'),
          lessonsButton = document.querySelector('#lessonsButton'),
          mentorButton = document.querySelector('#mentorButton'),
-         contactsModal = document.querySelector('#contactsModal'),
+         aboutusButton = document.querySelector('#aboutusButton'),
+         callToActionButton = document.querySelectorAll('.button');
+
+   const contactsModal = document.querySelector('#contactsModal'),
          lessonsModal = document.querySelector('#lessonsModal'),
          mentorModal = document.querySelector('#mentorModal'),
-         modalCloseButton = document.querySelectorAll('.modal__inner_close'),
+         aboutusModal = document.querySelector('#aboutusModal'),
+         formModal = document.querySelector('#formModal');
+
+   const modalCloseButton = document.querySelectorAll('.modal__inner_close'),
          modalOverlay = document.querySelectorAll('.modal__overlay');
+
 
    contactsButton.addEventListener('click', () => {
       contactsModal.style.display = 'flex';
+      modalSectionOpened = 1;
+      checkModalWindowsState();
    })
 
    lessonsButton.addEventListener('click', () => {
       lessonsModal.style.display = 'flex';
+      modalSectionOpened = 1;
+      checkModalWindowsState();
    })
 
    mentorButton.addEventListener('click', () => {
       mentorModal.style.display = 'flex';
+      modalSectionOpened = 1;
+      checkModalWindowsState();
+   })
+
+   aboutusButton.addEventListener('click', () => {
+      aboutusModal.style.display = 'flex';
+      modalSectionOpened = 1;
+      checkModalWindowsState();
+   })
+
+   callToActionButton.forEach(button => {
+      button.addEventListener('click', () => {
+         formModal.style.display = 'flex';
+         modalFormOpened = 1;
+         checkModalWindowsState();
+      })
    })
 
    /* closing modal windows */
    modalOverlay.forEach(overlay => {
       overlay.addEventListener('click', () => {
-         overlay.parentElement.style.display = ''
+         let currentModal = overlay.parentElement;
+         currentModal.style.display = '';
+
+         if (currentModal.classList.contains('modalSection')) {
+            modalSectionOpened = 0;
+            checkModalWindowsState();
+         }
+         if (currentModal.classList.contains('modalForm')) {
+            modalFormOpened = 0;
+            checkModalWindowsState();
+         }
       })
    })
 
    modalCloseButton.forEach(button => {
       button.addEventListener('click', () => {
-         const currentModal = button.parentElement.parentElement;
+         let currentModal = button.parentElement.parentElement;
          currentModal.style.display = '';
+         
+         if (currentModal.classList.contains('modalSection')) {
+            modalSectionOpened = 0;
+            checkModalWindowsState();
+         }
+         if (currentModal.classList.contains('modalForm')) {
+            modalFormOpened = 0;
+            checkModalWindowsState();
+         }
       })
-   })
+   });
 
    // enabling / disabling scrolling events
-   const disableScroll = () => {
-      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-      const body = document.body;
-      body.style.position = 'fixed';
-      body.style.top = `-${scrollY}`;
-    };
-    const enableScroll = () => {
-      const body = document.body;
-      const scrollY = body.style.top;
-      body.style.position = '';
-      body.style.top = '';
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
-    window.addEventListener('scroll', () => {
-      document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-    });
+   let scrollbarWidth;
+
+   function getScrollbarWidth() {
+
+      // Creating invisible container
+      const outer = document.createElement('div');
+      outer.style.visibility = 'hidden';
+      outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+      outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+      document.body.appendChild(outer);
+    
+      // Creating inner element and placing it in the container
+      const inner = document.createElement('div');
+      outer.appendChild(inner);
+    
+      // Calculating difference between container's full width and the child width
+      scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+    
+      // Removing temporary elements from the DOM
+      outer.parentNode.removeChild(outer);
+    
+      return scrollbarWidth;
+   }
+   getScrollbarWidth();
+   console.log(scrollbarWidth);
+
+
+   const disableBodyScroll = function() {
+      document.body.style =
+      `overflow-y:hidden; position: relative; margin-right: ${scrollbarWidth}px;`;
+   }
+   const enableBodyScroll = () => {
+      document.body.style = ""
+   }
+
+
+   // Defining modal conditions
+   let modalSectionOpened = 0, modalFormOpened = 0;
+
+   function checkModalWindowsState() {
+      if (modalSectionOpened || modalFormOpened) {
+         disableBodyScroll()
+      } else {
+         enableBodyScroll()
+      }
+   }
+
+
 })
